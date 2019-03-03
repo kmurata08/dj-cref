425012268e9d0efbc001f92f86d90c45da768dab
Canon11 <mrt014kzm@gmail.com>
2019-03-03 23:38:44 +0900

    add type args

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-type', type=str, help='the type of django generic view.')
+
     args = parser.parse_args()
-    print(args)
+
+    if not args.type:
+        print('args.type is undefined.')
 
 
 if __name__ == '__main__':
