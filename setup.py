1de4acf0cf4ab0edb14c33022c3ce86eb9f64f24
Canon11 <mrt014kzm@gmail.com>
2019-03-06 02:01:43 +0900

    add license

--- setup.py
+++ setup.py
@@ -5,15 +5,17 @@ with open('requirements.txt') as requirements_file:
     install_requirements = requirements_file.read().splitlines()
 
 setup(
-    name="dj-clef",
+    name="dj-cref",
     version="0.0.1",
     description="The django generic view output unit",
-    author="Canon11",
+    license="MIT",
+    author="Kazuma Murata",
     packages=find_packages(),
     install_requires=install_requirements,
+    keywords="django,generic,output",
     entry_points={
         "console_scripts": [
-            "dj-clef=command.main:main",
+            "dj-cref=command.main:main",
         ]
     },
     classifiers=[
