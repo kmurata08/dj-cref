1d6ef35757c0246f88b88b4e8002d67a86a28593
Canon11 <mrt014kzm@gmail.com>
2019-03-06 02:55:29 +0900

    add README to description

--- setup.py
+++ setup.py
@@ -4,15 +4,22 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as requirements_file:
     install_requirements = requirements_file.read().splitlines()
 
+try:
+    with open('README.md') as f:
+        readme = f.read()
+except IOError:
+    readme = ''
+
 setup(
     name="dj-cref",
-    version="0.0.5",
+    version="0.0.6",
     description="The django generic view output unit",
     license="MIT",
     author="Kazuma Murata",
     author_email="mrt014kzm@gmail.com",
     packages=find_packages(exclude=['venv/', 'Makefile']),
     install_requires=install_requirements,
+    long_description=readme,
     keywords="django,generic,output",
     url="https://github.com/Canon11/dj-cref",
     entry_points={
